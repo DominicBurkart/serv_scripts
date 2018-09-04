e2b07d396071b4a01063113a8e5f45c445536052
Dominic Burkart <1dominicb@gmail.com>
2018-09-03 21:56:39 -0400

    add printout for movestreams.py

--- movestreams.py
+++ movestreams.py
@@ -8,13 +8,18 @@ outdir = sys.argv[2]
 
 forty_eight = 48 * 60 * 60
 
-print("Copying files that haven't been edited for at least 48 hours.")
+print("Movestreams started. Copying files that haven't been edited for at least 48 hours.")
 
+found = False
 for root, dirs, files in os.walk(indir):
     for f in files:
         old = os.path.join(root, f)
         if time.time() - os.path.getmtime(old) > forty_eight:
             print("Moving file: " + str(old))
             shutil.move(old, os.path.join(outdir, f))
+            found = True
+
+if not found:
+    print("No files to copy found.")
 
 print("Movestreams complete.")
