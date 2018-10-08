785eda7480d776fb22f73d98d09f219b3cdd238b
Dominic Burkart <1dominicb@gmail.com>
2018-10-08 15:07:55 -0400

    set movestreams to send anything older than 25 hours

--- movestreams.py
+++ movestreams.py
@@ -1,20 +1,21 @@
-import sys
-import subprocess
-import time
 import os
+import subprocess
+import sys
+import time
 
 indir = sys.argv[1]
 outdir = sys.argv[2]
 
-two_days = 48 * 60 * 60
+TIME = 25 * 60 * 60
 
-print("Movestreams started. Copying files that haven't been edited for at least 48 hours.")
+print("Movestreams started. Copying files that were last updated " + str(TIME) + " seconds (" +
+      str(float(TIME) / (24 * 60 * 60)) + " days) ago.")
 
 found = False
 for root, dirs, files in os.walk(indir):
     for f in files:
         old = os.path.join(root, f)
-        if time.time() - os.path.getmtime(old) > two_days:
+        if time.time() - os.path.getmtime(old) > TIME:
             print("Moving file: " + str(old))
             subprocess.run("rsync -a " + old + " " + outdir + "; rm " + old, shell=True)
             found = True
